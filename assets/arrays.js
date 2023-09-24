42eaa4af656fc22c21b7f878d5a737b7935e19b3
NelsonKinyanjui <nelsonkinyanjui246@gmail.com>
2023-09-24 20:59:05 +0300

    use map and filter in Javascript

--- assets/arrays.js
+++ assets/arrays.js
@@ -1,4 +1,4 @@
-const fruits = ['mango','apple','banana','passion'];
+/* const fruits = ['mango','apple','banana','passion'];
  let fLen = fruits.length
  let text = '<ul>';
  for (i = 0; i < fLen; i++){
@@ -10,5 +10,13 @@ const fruits = ['mango','apple','banana','passion'];
     for (i = 0; i <= 100; i++){
       console.log('My Name is Collins');
 
+    }*/
+
+    function mCat(cat){
+      return cat.startsWith('m');
     }
+     const cat = ['monkey','monster','merlin','lion','beast'];
+
+     const filtered = cat.filter(mCat);
+     console.log(filtered);
  
