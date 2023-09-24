ed0b56da2a4b93da59196f3a3bbba1fadb2fb840
NelsonKinyanjui <nelsonkinyanjui246@gmail.com>
2023-09-24 21:23:09 +0300

    add example of array in use how arrays work together with DOM

--- assets/arrays.js
+++ assets/arrays.js
@@ -1,22 +1,27 @@
-/* const fruits = ['mango','apple','banana','passion'];
- let fLen = fruits.length
- let text = '<ul>';
- for (i = 0; i < fLen; i++){
-    text += '<li>' + fruits[i] +'</li>';
-
- } text += '</ul>';
-    console.log();
-
-    for (i = 0; i <= 100; i++){
-      console.log('My Name is Collins');
-
-    }*/
-
-    function mCat(cat){
-      return cat.startsWith('m');
-    }
-     const cat = ['monkey','monster','merlin','lion','beast'];
-
-     const filtered = cat.filter(mCat);
-     console.log(filtered);
+const contacts = [
+   "Chris:2232322",
+   "Sarah:3453456",
+   "Bill:7654322",
+   "Mary:9998769",
+   "Dianne:9384975",
+ ];
+ const para = document.querySelector("p");
+ const input = document.querySelector("input");
+ const btn = document.querySelector("button");
  
+ btn.addEventListener("click", () => {
+   const searchName = input.value.toLowerCase();
+   input.value = "";
+   input.focus();
+   para.textContent = "";
+   for (const contact of contacts) {
+     const splitContact = contact.split(":");
+     if (splitContact[0].toLowerCase() === searchName) {
+       para.textContent = `${splitContact[0]}'s number is ${splitContact[1]}.`;
+       break;
+     }
+   }
+   if (para.textContent === "") {
+     para.textContent = "Contact not found.";
+   }
+ });
